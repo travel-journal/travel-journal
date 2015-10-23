06194faf51c7eaa8ff0bfbdedc92a08ce48b3512
Jasmine Giang <jasminegiang@gmail.com>
2015-10-23 00:47:39 -0700

    Fix update user
    update with password confirmation

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -45,7 +45,7 @@ class UsersController < ApplicationController
   # PATCH/PUT /users/1.json
   def update
     respond_to do |format|
-      if @user.update(user_params)
+      if @user.update_with_password(user_params)
         format.html { redirect_to @user, notice: 'User was successfully updated.' }
         format.json { render :show, status: :ok, location: @user }
       else
@@ -73,6 +73,6 @@ class UsersController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def user_params
-      params.require(:user).permit(:username, :password, :firstname, :lastname, :emailaddress, :about)
+      params.require(:user).permit(:username, :password, :firstname, :lastname, :emailaddress, :about, :current_password)
     end
 end
