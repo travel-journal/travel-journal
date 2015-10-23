7e27e701bd2569b25716c4a50680eaf9d25280be
Jihui Jin <thejihuijin@gmail.com>
2015-10-22 20:21:37 -0700

    installed carrierwave

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,10 +3,13 @@ class Post < ActiveRecord::Base
   validates :trip, :presence => true
   validates :date, :presence => true
   validates :like_count, :presence => true
-  valides :like_count, :numericality => {
+  validates :like_count, :numericality => {
     :only_integer => true,
     :greater_than_or_equal_to => 0
   }
+
+  
   belongs_to :user
   belongs_to :trip
+  mount_uploader :image, ImageUploader
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151015082603) do
+ActiveRecord::Schema.define(version: 20151023030547) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -28,6 +28,7 @@ ActiveRecord::Schema.define(version: 20151015082603) do
     t.datetime "updated_at", null: false
     t.integer  "user_id"
     t.integer  "trip_id"
+    t.string   "image"
   end
 
   add_index "posts", ["trip_id"], name: "index_posts_on_trip_id", using: :btree
