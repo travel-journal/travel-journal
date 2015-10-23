24bdc70ccd3896a280b3715d570f20314407e1fe
Jasmine Giang <jasminegiang@gmail.com>
2015-10-22 23:02:45 -0700

    Fix edit registration form
    Make correct api requests on update and cancel

--- config/routes.rb
+++ config/routes.rb
@@ -11,12 +11,12 @@ Rails.application.routes.draw do
     get '/users/password/edit', to: 'devise/passwords#edit', as: 'edit_user_password'
     patch '/api/users/password', to: 'devise/passwords#update'
     put '/api/users/password', to: 'devise/passwords#update'
-    get 'api/users/cancel', to: 'devise/registrations#cancel', as: 'cancel_user_registration'
+    delete 'api/users/cancel', to: 'devise/registrations#cancel', as: 'cancel_user_registration'
     post '/api/users', to: 'devise/registrations#create', as: 'user_registration'
     get '/users/sign_up', to: 'devise/registrations#new', as: 'new_user_registration'
     get '/users/edit', to: 'devise/registrations#edit', as: 'edit_user_registration'
-    patch '/api/users', to: 'devise/registrations#update'
-    put '/api/users', to: 'devise/registrations#update'
+    patch '/api/users', to: 'devise/registrations#update'    
+    put '/api/users', to: 'devise/registrations#update', as: 'update_user_registration'
     delete '/api/users', to: 'devise/registrations#destroy'
   end
   
@@ -24,7 +24,7 @@ Rails.application.routes.draw do
   # they don't interact with models so they don't get /api prefix
   scope 'api', except: [:new, :edit] do
     # needed for user view at the moment
-    resources :users
+    #resources :users
 
     resources :trips
     resources :posts  
@@ -34,8 +34,8 @@ Rails.application.routes.draw do
   # and respond with it
 
   # needed for user view at the moment
-  get '/users/new', to: 'users#new', as: 'new_user'
-  get '/users/:id/edit', to: 'users#edit', as: 'edit_user'
+  #get '/users/new', to: 'users#new', as: 'new_user'
+  #get '/users/:id/edit', to: 'users#edit', as: 'edit_user'
 
   get '/trips/new', to: 'trips#new', as: 'new_trip'
   get '/trips/:id/edit', to: 'trips#edit', as: 'edit_trip'
