e8e2029b0e0cd4e53fa6491a3464c7ed093c2302
Claire Watanabe <claireleilani@gmail.com>
2015-10-23 01:12:18 -0700

    Added search for trips

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -2,4 +2,8 @@ class Trip < ActiveRecord::Base
     belongs_to :user, :foreign_key => :user_id
     has_many :posts
     validates :title, :about, :start_date, :end_date, presence: true
+
+    def self.search(search)
+      where("title LIKE ? OR about LIKE ?", "%#{search}%", "%#{search}%")
+	end
 end
