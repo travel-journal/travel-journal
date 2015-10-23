fcc066e478126b74657160cee90ed9361c7f46f7
Dhruv Arora <dhruvarora@berkeley.edu>
2015-10-22 20:34:45 -0700

    Fix start date and end date for trips
    This commit contains migrations to rename startDate and endDate in the
    trips models

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151022075248) do
+ActiveRecord::Schema.define(version: 20151023032235) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -37,8 +37,8 @@ ActiveRecord::Schema.define(version: 20151022075248) do
     t.string   "title"
     t.integer  "user_id"
     t.text     "about"
-    t.date     "startDate"
-    t.date     "endDate"
+    t.date     "start_date"
+    t.date     "end_date"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
