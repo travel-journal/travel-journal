d6a6508452947d48ca6aee97de9db804e59564ad
Jihui Jin <thejihuijin@gmail.com>
2015-10-22 23:32:31 -0700

    Like button implement

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,11 +2,13 @@ class Post < ActiveRecord::Base
   validates :title, :presence => true
   #validates :trip, :presence => true
   validates :date, :presence => true
-  validates :like_count, :presence => true
-  validates :like_count, :numericality => {
-    :only_integer => true,
-    :greater_than_or_equal_to => 0
-  }
+  
+  # should initialize to 0 in controller?
+  #validates :like_count, :presence => true
+  #validates :like_count, :numericality => {
+  #  :only_integer => true,
+  #  :greater_than_or_equal_to => 0
+  #}
 
   
   belongs_to :user
