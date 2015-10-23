a9593dd5e13356dd74a7c9e8a833f7e9ffb20da1
Dhruv Arora <dhruvarora@berkeley.edu>
2015-10-22 18:06:58 -0700

    Implement authentication with additional fields
    This commit modifies the current authentication structure to
    authenticate based on username instead of email. It adds additional
    fields to sign up, and makes stylized changes to user views. It removes
    the resources for user routing, and also changes the
    requirements for password from 8 characters to 5.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,16 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  before_filter :configure_permitted_parameters, if: :devise_controller?
+
+  def after_sign_in_path_for(resource)
+    trips_path(current_user)
+  end
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:username, :email, :password, :password_confirmation, :first_name, :last_name, :about) }
+    devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:username, :email, :password, :password_confirmation, :first_name, :last_name, :about) }
+  end
 end
