7dbcc44b92dc657899463205a78ce724b0cc243f
Jihui Jin <thejihuijin@gmail.com>
2015-10-23 01:10:26 -0700

    Remove trip column from Posts

--- db/schema.rb
+++ db/schema.rb
@@ -11,14 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151023032235) do
+ActiveRecord::Schema.define(version: 20151023075905) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "posts", force: :cascade do |t|
     t.string   "title"
-    t.string   "trip"
     t.text     "caption"
     t.string   "location"
     t.date     "date"
