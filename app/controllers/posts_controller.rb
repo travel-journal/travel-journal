2a89f517162867a54a2f2023309dc73a20e8d8ab
Jasmine Giang <jasminegiang@gmail.com>
2015-10-23 18:23:12 -0700

    Functional UI
    All links direct to correct views

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -53,8 +53,9 @@ class PostsController < ApplicationController
   def update
     respond_to do |format|
       if @post.update(post_params)
-        format.html { redirect_to @post, notice: 'Post was successfully updated.' }
-        format.json {render day_posts_path({:date => p.date, :trip_id => p.trip_id}), status: :ok, location: @post}
+        format.html { redirect_to day_posts_path({:date => @post.date, :trip_id => @post.trip_id}), notice: 'Post was successfully created.' }
+
+        #format.html { redirect_to @post, notice: 'Post was successfully updated.' }
         # to display the post after updating it
         #format.json { render :show, status: :ok, location: @post }
       else
--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -15,8 +15,8 @@ class TripsController < ApplicationController
   # GET /trips/1
   # GET /trips/1.json
   def show
-    @posts_of_trip = Post.where(:user_id => current_user.id).order("date")
-    # @posts_of_trip = Post.where(:trip_id => params[:id], :user_id => current_user.id).order("created_at DESC")
+    #@posts_of_trip = Post.where(:user_id => current_user.id).order("date")
+    @posts_of_trip = Post.where(:trip_id => params[:id], :user_id => current_user.id).order("created_at DESC")
 
 
     previous = nil
@@ -47,7 +47,7 @@ class TripsController < ApplicationController
 
     respond_to do |format|
       if @trip.save
-        format.html { redirect_to @trip, notice: 'Trip was successfully created.' }
+        format.html { redirect_to trips_path, notice: 'Trip was successfully created.' }
         format.json { render :show, status: :created, location: @trip }
       else
         format.html { render :new }
@@ -61,7 +61,7 @@ class TripsController < ApplicationController
   def update
     respond_to do |format|
       if @trip.update(trip_params)
-        format.html { redirect_to @trip, notice: 'Trip was successfully updated.' }
+        format.html { redirect_to trips_path, notice: 'Trip was successfully updated.' }
         format.json { render :show, status: :ok, location: @trip }
       else
         format.html { render :edit }
