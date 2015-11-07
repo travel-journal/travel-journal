3aadb4a15e661d30fba436054302ff5f3d7c6b4c
Jihui Jin <thejihuijin@gmail.com>
2015-11-06 19:59:33 -0800

    test application

--- config/application.rb
+++ config/application.rb
@@ -8,16 +8,6 @@ require 'carrierwave/orm/activerecord'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-# Devise and testing
-ActionDispatch::Callbacks.after do      
-  # Reload the factories
-  return unless (Rails.env.development? || Rails.env.test?)
-
-  unless FactoryGirl.factories.blank? # first init will load factories, this should only run on subsequent reloads
-    FactoryGirl.factories.clear
-    FactoryGirl.find_definitions
-  end
-end 
 
 module TravelJournal
   class Application < Rails::Application
@@ -37,5 +27,15 @@ module TravelJournal
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
   
+    config.generators do |g|
+      g.test_framework :rspec,
+        :fixtures => true,
+        :view_specs => false,
+        :helper_specs => false,
+        :routing_specs => false,
+        :controller_specs => true,
+        :request_specs => true
+      g.fixture_replacement :factory_girl, :dir => "spec/factories"
+    end
   end
 end
