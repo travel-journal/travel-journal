814e89cbe09626260c608be780183d2ef8898ce7
Jihui Jin <thejihuijin@gmail.com>
2015-11-06 20:07:07 -0800

    remove bad lines of code from application

--- config/application.rb
+++ config/application.rb
@@ -9,15 +9,15 @@ require 'carrierwave/orm/activerecord'
 Bundler.require(*Rails.groups)
 
 # Devise and testing
-ActionDispatch::Callbacks.after do      
-  # Reload the factories
-  return unless (Rails.env.development? || Rails.env.test?)
-
-  unless FactoryGirl.factories.blank? # first init will load factories, this should only run on subsequent reloads
-    FactoryGirl.factories.clear
-    FactoryGirl.find_definitions
-  end
-end 
+#ActionDispatch::Callbacks.after do      
+#  # Reload the factories
+#  return unless (Rails.env.development? || Rails.env.test?)
+#
+#  unless FactoryGirl.factories.blank? # first init will load factories, this should only run on subsequent reloads
+#    FactoryGirl.factories.clear
+#    FactoryGirl.find_definitions
+#  end
+#end 
 
 module TravelJournal
   class Application < Rails::Application
