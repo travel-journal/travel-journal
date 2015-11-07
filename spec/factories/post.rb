2492164cd89e0614d1aa6b4609ab862900c0a865
Claire Watanabe <claireleilani@gmail.com>
2015-11-06 17:18:51 -0800

    Working post factory

--- spec/factories/post.rb
+++ spec/factories/post.rb
@@ -5,6 +5,9 @@ FactoryGirl.define do
   factory :post do |f|
     f.title { Faker::Company.catch_phrase }
     f.date { Date.today }
+    f.time { Faker::Time.backward(14, :evening) }
+    f.location { Faker::Address.country }
+    f.caption { Faker::Hacker.say_something_smart }
 
     factory :trip_post do |f|
     	f.trip_id { Faker::Number.number(2) }
