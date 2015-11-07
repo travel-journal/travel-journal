58899f412dd678b4debba83c4f7b3dd043c772c7
Jihui Jin <thejihuijin@gmail.com>
2015-11-06 20:03:47 -0800

    comment out code

--- config/application.rb
+++ config/application.rb
@@ -8,6 +8,16 @@ require 'carrierwave/orm/activerecord'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
+## Devise and testing
+#ActionDispatch::Callbacks.after do      
+#  # Reload the factories
+#  return unless (Rails.env.development? || Rails.env.test?)
+#
+#  unless FactoryGirl.factories.blank? # first init will load factories, this should only run on subsequent reloads
+#    FactoryGirl.factories.clear
+#    FactoryGirl.find_definitions
+#  end
+#end 
 
 module TravelJournal
   class Application < Rails::Application
