0907608cd1667cc02226b6b568263a0294924cf5
Claire Watanabe <claireleilani@gmail.com>
2015-11-06 17:07:25 -0800

    Fix to user_sign_in test

--- spec/requests/user_sign_in_spec.rb
+++ spec/requests/user_sign_in_spec.rb
@@ -1,7 +1,12 @@
-require "spec_helper"
+require 'rails_helper'
+require 'spec_helper'
+require 'rspec/mocks/standalone'
+require 'faker'
+include Devise::TestHelpers
 
 describe "user sign in" do
   it "allows users to sign in after they have registered" do
+  	FactoryGirl.create(:user, username: "Nishu", password:"hellobear")
 
     visit "/users/sign_in"
 
