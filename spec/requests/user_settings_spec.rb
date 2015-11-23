613520c47f54247c094f9f3f354f1ed1067fa41a
Claire Watanabe <claireleilani@gmail.com>
2015-11-23 12:09:48 -0800

    Delete User + Homepage tests

--- spec/requests/user_settings_spec.rb
+++ spec/requests/user_settings_spec.rb
@@ -2,24 +2,32 @@ require "spec_helper"
 require 'rails_helper'
 
 describe "user settings" do
-  it "ensure that user setting page updates correctly" do
-  	FactoryGirl.create(:full_user, username: "Claire", password:"hellobear", first_name: "Nishu", last_name: "Tanay", about: "I am a Tanay Nishu combo")
+
+  before :each do
+    FactoryGirl.create(:full_user, username: "Claire", password:"hellobear", first_name: "Nishu", last_name: "Tanay", about: "I am a Tanay Nishu combo")
 
     visit "/users/sign_in"
 
-    fill_in "Username",		:with => "Claire"
-    fill_in "Password", 	:with => "hellobear"
+    fill_in "Username",   :with => "Claire"
+    fill_in "Password",   :with => "hellobear"
 
     click_button "Sign in"
 
     #visit "/users/edit"
     click_link "Update profile"
+  end
 
+  it "has correct info" do
+  	
     page.should have_content("Update Profile")
     page.should have_content("Nishu")
     page.should have_content("Tanay")
-    #page.should have_content("hello@world.com")
     page.should have_content("I am a Tanay Nishu combo")
     
+  end
+
+  it "able to delete user" do
+    click_button "Delete"
+    page.should have_content("Bye!")
   end
 end
