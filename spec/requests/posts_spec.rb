b5a08c7c18be4b03157235b03cbe241fc37316ae
Claire Watanabe <claireleilani@gmail.com>
2015-11-24 13:52:28 -0800

    finished posts tests

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -11,13 +11,13 @@ describe "Posts (Integration Tests)" do
     fill_in "Password",   :with => "tester"
     click_button "Sign in"
 
-	click_link "Make a new trip"
-	fill_in "Title",   :with => "This is my test Title"
-	fill_in "About",   :with => "This is my test About"
-	click_button "Submit"
-	page.should have_content("Trip was successfully created.")
+  	click_link "Make a new trip"
+  	fill_in "Title",   :with => "This is my test Title"
+  	fill_in "About",   :with => "This is my test About"
+  	click_button "Submit"
+  	page.should have_content("Trip was successfully created.")
 
-	click_link "Expand"
+  	click_link "Expand"
   end
 
   describe "Creating" do
@@ -39,6 +39,39 @@ describe "Posts (Integration Tests)" do
 
       click_button "Create Post"
       page.should have_content("Post was successfully created.")
+    end
+  end
+
+  describe "Editing, Deleting, & Searching" do 
+    before :each do
+      click_link "Add post"
+      fill_in "Title",   :with => "BestPost"
+      fill_in "Caption",   :with => "AboutBestPost"
+      fill_in "Location",   :with => "Berkeley"
+      select '2010', from: "post_date_1i"
+      select 'January', from: "post_date_2i"
+      select '1', from: "post_date_3i"
+      select '00', from: "post_time_4i"
+      select '00', from: "post_time_5i"
+
+      click_button "Create Post"
+    end
+
+    it "able to get to edit form" do
+      click_link "Edit"
+      page.should have_content("Edit Post")
+    end
+    
+    it "able to edit" do
+      click_link "Edit"
+      fill_in "Title",   :with => "Cherry"
+      click_button "Update Post"
+      page.should have_content("Cherry")
+    end
+
+    it "able to delete" do
+      click_link "Delete"
+      page.should have_content("Post was successfully destroyed.")
     end
   end
 end
